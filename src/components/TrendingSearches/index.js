121b10fe59709edccaa6d13a05f2eb9377d3c2b7
Francisco Ramos <rfrancisco.dev@gmail.com>
2023-05-31 09:04:27 -0300

    add skeleton trending searches

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,21 +1,46 @@
 import Category from "components/Category";
 import React, { useEffect, useState } from "react";
-import { getTrendingSearches } from "services/getTrendingSearches";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
+import { getTrendingSearches } from "services/getTrendingSearches";
 import "./TrendingSearches.css";
 
 export default function TrendingSearches() {
   const [trends, setTrends] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getTrendingSearches().then(setTrends);
+    getTrendingSearches().then((trends) => {
+      setTrends(trends);
+      setLoading(false);
+    });
   }, []);
 
   return (
     <div className="trend-container">
-      {trends.map((trend, index) => (
-        <Category key={index} name={trend} />
-      ))}
+      {loading ? (
+        <SkeletonLoadingTrends count={12} />
+      ) : (
+        trends.map((trend, index) => <Category key={index} name={trend} />)
+      )}
     </div>
+  );
+}
+
+function SkeletonLoadingTrends({ count }) {
+  return (
+    <>
+      {Array.from({ length: count }).map((_, index) => (
+        <Skeleton
+          key={index}
+          baseColor="#666666"
+          highlightColor="#b1b1b1"
+          borderRadius={30}
+          width={100}
+          height={30}
+        />
+      ))}
+    </>
   );
 }
